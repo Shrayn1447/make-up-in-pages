a28c0a7c5a46a9f37d9645a9ad4886d67b7e6b40
Alex <scanel2004@gmail.com>
2024-02-04 17:41:38 +0700

    mucommit

--- (Next.js)Сайт Технологий для детей/src/components/Main.jsx
+++ (Next.js)Сайт Технологий для детей/src/components/Main.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
 function Grid({ name, text }) {
   return (
     <div className="div w-[440px] h-[500px] transition bg-white rounded-xl pt-[50px] pl-[20px] pr-[20px]">
       <img src={'./img/software.svg'} alt="Картинка" />
       <h1 className="font-bold text-[25px] pt-[10px] w-[250px] h-[90px]">{name}</h1>
-        <p className="text-[20px] text antialiased pt-[10px] mt-[70px]">{text}</p>
+        <p className=" text antialiased pt-[10px] mt-[70px]">{text}</p>
     </div>
   );
 }
@@ -15,7 +15,7 @@ export default function Main() {
     <div>
       <h1 className="text-[60px] font-bold text-center mt-[40px] antialiased">Nossos serviços</h1>
       <h3 className="text-[25px] antialiased text-gray-400 text-center">
-        Soluções Tecnológicas Personalizadas para Impulsionar o seu Negócio
+        SoluçTecnológicas Personalizadas para Impulsionar o seu Negócio
       </h3>
       <div className="flex gap-[40px] justify-center mt-[40px]">
         <Grid
